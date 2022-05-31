f23917630b277106237befed6706e15c3ba05b99
renhiyama <sayantan.das.gg@gmail.com>
2022-05-31 13:01:37 +0530

    maybe a fix?

--- reewrite/reetoolkit/script.sh
+++ reewrite/reetoolkit/script.sh
@@ -18,7 +18,7 @@ if [ -f ~/.zshrc ]; then
     echo "alias reejs='node ~/.reejs/toolkit/index.js'" >> ~/.zshrc
 fi
 
-echo "[INSTALL] Successfully added alias to your available shell configuration files. If you don't use bash or zsh, you can add it manually to your shell configuration file"
-echo "\nalias reejs='node ~/.reejs/toolkit/index.js'\n"
+echo "[INSTALL] Successfully added alias to your available shell configuration files. If you don't use bash or zsh, you can add the below line manually to your shell configuration file:"
+echo "\\nalias reejs='node ~/.reejs/toolkit/index.js'\\n"
 
 echo "[INSTALL] Reetoolkit installed! Run 'reejs init reejs-app' to start using it!\nLearn more about it here: <link coming soon!>"
