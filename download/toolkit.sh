dbf2a41f8422b7eb104929231280361544be0579
renhiyama <sayantan.das.gg@gmail.com>
2022-05-31 13:06:31 +0530

    echo to printf

--- download/toolkit.sh
+++ download/toolkit.sh
@@ -1,26 +1,26 @@
 if [ -d ~/.reejs ]; then
-  echo "[INSTALL] reetoolkit already installed"
+  printf "[INSTALL] reetoolkit already installed"
   exit 0
 fi
 
-echo "[INSTALL] Initializing reetoolkit on $HOME"
+printf "[INSTALL] Initializing reetoolkit on $HOME"
 mkdir -p ~/.reejs/toolkit
-echo "[INSTALL] Downloading reetoolkit"
+printf "[INSTALL] Downloading reetoolkit"
 curl -s https://raw.githubusercontent.com/rovelstars/reejs/master/reewrite/reetoolkit/index.js > ~/.reejs/toolkit/index.js
-echo "[INSTALL] Installing reetoolkit"
-echo "[INSTALL] Adding alias to your shell configuration file and also the current shell"
+printf "[INSTALL] Installing reetoolkit"
+printf "[INSTALL] Adding alias to your shell configuration file and also the current shell"
 
 # add alias to .bashrc and .zshrc if exists
 if [ -f ~/.bashrc ]; then
-    echo "alias reejs='node ~/.reejs/toolkit/index.js'" >> ~/.bashrc
+    printf "alias reejs='node ~/.reejs/toolkit/index.js'" >> ~/.bashrc
 fi
 if [ -f ~/.zshrc ]; then
-    echo "alias reejs='node ~/.reejs/toolkit/index.js'" >> ~/.zshrc
+    printf "alias reejs='node ~/.reejs/toolkit/index.js'" >> ~/.zshrc
 fi
 
-echo "[INSTALL] Successfully added alias to your available shell configuration files. If you don't use bash or zsh, you can add the below line manually to your shell configuration file:"
-echo "\\nalias reejs='node ~/.reejs/toolkit/index.js'\\n"
+printf "[INSTALL] Successfully added alias to your available shell configuration files. If you don't use bash or zsh, you can add the below line manually to your shell configuration file:"
+printf "\\nalias reejs='node ~/.reejs/toolkit/index.js'\\n"
 
 alias reejs='node ~/.reejs/toolkit/index.js'
 
-echo "[INSTALL] Reetoolkit installed! Run 'reejs init reejs-app' to start using it!\nLearn more about it here: <link coming soon!>"
+printf "[INSTALL] Reetoolkit installed! Run 'reejs init reejs-app' to start using it!\nLearn more about it here: <link coming soon!>"
