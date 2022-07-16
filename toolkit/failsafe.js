83dba7f6cae67185c57d130f074c84e7e0be0fa4
Ren Hiyama <renhiyama@rovelstars.com>
2022-07-16 11:51:41 +0530

    this should work

--- toolkit/failsafe.js
+++ toolkit/failsafe.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 //This is the file that installs the dependencies for the toolkit so toolkit can be used. Exactly follows the specification of cmds/install.js
 
-import {spawn} from "child_process";
+import {exec} from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(__dirname);
-spawn("node ./failsafe/install.js && node ./failsafe/link.js", {stdio: "inherit", detached: false,cwd: __dirname});
+exec(`node ./failsafe/install.js && ./failsafe/link.js`, {cwd: __dirname},()=>{
+  console.log("[INFO] Installing and linking dependencies for toolkit!");
+});
